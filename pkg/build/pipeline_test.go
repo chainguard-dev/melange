4a4086672a434554bead7b5bdf5afdea05e790da
Ariadne Conill <ariadne@dereferenced.org>
2022-03-23 21:08:17 +0000

    add tests for variable substitution

---
+++ pkg/build/pipeline_test.go
@@ -0,0 +1,33 @@
+// Copyright 2022 Chainguard, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package build
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_mutateStringFromMap(t *testing.T) {
+	keys := map[string]string{
+		"${{inputs.foo}}": "foo",
+		"${{inputs.bar}}": "bar",
+	}
+
+	input1 := "${{inputs.foo}} ${{inputs.baz}}"
+	output1 := mutateStringFromMap(keys, input1)
+
+	require.Equal(t, output1, "foo ", "bogus variable substitution not deleted")
+}

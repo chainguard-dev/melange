6919dede82caa69b474f136c6b5d538c569a317f
Adrian Mouat <adrian@chainguard.dev>
2022-04-05 15:41:44 +0100

    Add script for running melange.

---
+++ hack/run-devenv.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Copyright 2022 Chainguard, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+function run() {
+
+    # Can we dogfood and do the following with apkgo?
+
+    go install golang.org/x/tools/cmd/goimports@latest 
+    export PATH=$PATH:/root/go/bin
+    make melange
+    make install
+
+    melange "$@"
+}
+
+function docker_run() {
+    docker run --rm -w /melange -v $(pwd):/melange --entrypoint /melange/hack/run-devenv.sh apko-inception:latest run $@
+}
+
+case "$1" in
+    "run")
+        run ${@:4};;
+    *)
+	docker_run $@;;
+esac
+

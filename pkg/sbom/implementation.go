9460524e9d5048e12c05d7fd583326e45df656aa
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-11-23 21:27:37 -0600

    Add k8s bnom attribution
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/sbom/implementation.go
+++ pkg/sbom/implementation.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Some of this code is based on the bom tool scan code originally
+// found at https://github.com/kubernetes-sigs/bom/blob/main/pkg/spdx/implementation.go
+
 package sbom
 
 import (

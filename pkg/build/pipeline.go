08dc17acc5ad25efafd95469bebd8c016f11f1c2
Ariadne Conill <ariadne@dereferenced.org>
2023-01-23 20:15:27 +0000

    pipeline: set working directory when evaluating pipeline "runs" entries

--- pkg/build/pipeline.go
+++ pkg/build/pipeline.go
@@ -227,9 +227,14 @@ func (p *Pipeline) evalRun(ctx *PipelineContext) error {
 	p.With = mutateWith(ctx, p.With)
 	p.dumpWith()
 
+	workdir := "/home/build"
+	if p.WorkDir != "" {
+		workdir = mutateStringFromMap(p.With, p.WorkDir)
+	}
+
 	fragment := mutateStringFromMap(p.With, p.Runs)
 	sys_path := "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-	script := fmt.Sprintf("#!/bin/sh\nset -e\nexport PATH=%s\n%s\nexit 0\n", sys_path, fragment)
+	script := fmt.Sprintf("#!/bin/sh\nset -e\nexport PATH=%s\ncd %s\n%s\nexit 0\n", sys_path, workdir, fragment)
 	command := []string{"/bin/sh", "-c", script}
 	config := ctx.Context.WorkspaceConfig()
 

850da39e15bfe814ac1fa840bacda62e3b866f64
Ariadne Conill <ariadne@dereferenced.org>
2023-04-06 13:55:07 -0500

    build: remove actualArchs variable, no longer used
    Signed-off-by: Ariadne Conill <ariadne@dereferenced.org>

--- pkg/cli/build.go
+++ pkg/cli/build.go
@@ -147,7 +147,6 @@ func BuildCmd(ctx context.Context, archs []apko_types.Architecture, base_opts ..
 	// Yes, this happens.  Really.
 	// https://github.com/distroless/nginx/runs/7219233843?check_suite_focus=true
 	bcs := []*build.Context{}
-	actualArchs := make([]string, 0, len(archs))
 	for _, arch := range archs {
 		opts := append(base_opts, build.WithArch(arch), build.WithBuiltinPipelineDirectory(BuiltinPipelineDir))
 
@@ -159,7 +158,6 @@ func BuildCmd(ctx context.Context, archs []apko_types.Architecture, base_opts ..
 			return err
 		}
 
-		actualArchs = append(actualArchs, arch.ToAPK())
 		bcs = append(bcs, bc)
 	}
 

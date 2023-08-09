b20662cecbb0a4308cbaa35c88f2ac7dfbc668f0
Jesse Portnoy <jesse@packman.io>
2023-08-09 18:33:46 +0100

    Fixes go-lint warnings:
    pkg/container/runner.go:29:6: exported type Runner should have comment or be unexported
    pkg/container/runner.go:47:6: exported type Loader should have comment or be unexported
    
    Signed-off-by: Jesse Portnoy <jesse@packman.io>

--- pkg/container/runner.go
+++ pkg/container/runner.go
@@ -26,6 +26,7 @@ import (
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 )
 
+// Runner interface
 type Runner interface {
 	Name() string
 	TestUsability(ctx context.Context) bool
@@ -44,6 +45,7 @@ type Runner interface {
 	WorkspaceTar(ctx context.Context, cfg *Config) (io.ReadCloser, error)
 }
 
+// Loader interface
 type Loader interface {
 	LoadImage(ctx context.Context, layer v1.Layer, arch apko_types.Architecture, bc *apko_build.Context) (ref string, err error)
 }

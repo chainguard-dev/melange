b070ac595f91d088be7ba240c4d1f29a8f0c5b85
Jason Hall <jason@chainguard.dev>
2024-06-24 21:47:20 -0400

    log it real good
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- pkg/linter/linter.go
+++ pkg/linter/linter.go
@@ -305,7 +305,7 @@ func worldWriteableLinter(ctx context.Context, pkgname string, fsys fs.FS) error
 		mode := info.Mode()
 		if mode&0002 != 0 {
 			if mode&0111 != 0 {
-				return fmt.Errorf("world-writeable executable file found in package (security risk): %s")
+				return fmt.Errorf("world-writeable executable file found in package (security risk): %s", path)
 			}
 			return fmt.Errorf("world-writeable file found in package: %s", path)
 		}

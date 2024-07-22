8d62e15dce6515e6906734b81833e3c4eeb0c503
Luca Di Maio <luca.dimaio1@gmail.com>
2024-07-22 14:27:32 +0200

    fix(qemu): download a kernel image if the host does not provide one, update the init in order to load network drivers if needed
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- pkg/container/qemu_init.sh
+++ pkg/container/qemu_init.sh
@@ -9,6 +9,11 @@ set -e
 /bin/mount -t devpts devpts -o noexec,nosuid,newinstance,ptmxmode=0666,mode=0620,gid=tty /dev/pts/
 /bin/mount --bind /dev/pts/ptmx /dev/ptmx
 
+# We can ignore this fail, if we use a kernel with kvm_guest.config, we won't need this
+# and network will work anyway
+# If this fails and we won't have network, the ifconfig command will fail anyway.
+/usr/sbin/modprobe virtio_net || :
+
 /bin/ifconfig lo up
 /bin/ifconfig eth0 10.0.2.15 netmask 255.255.255.0
 /bin/route add default gw 10.0.2.2 eth0

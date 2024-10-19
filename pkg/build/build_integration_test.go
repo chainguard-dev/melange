91857e2054f575a366d28c0cb080bfedf60f26ee
Dan Luhring <dluhring@chainguard.dev>
2024-10-18 20:22:23 -0400

    move sbom test inside build test
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

--- pkg/build/build_integration_test.go
+++ pkg/build/build_integration_test.go
@@ -63,57 +63,57 @@ func TestBuild_BuildPackage(t *testing.T) {
 				if err := b.BuildPackage(ctx); err != nil {
 					t.Fatalf("building package: %v", err)
 				}
-			})
 
-			t.Run("sbom correctness", func(t *testing.T) {
-				apkPath := filepath.Join(tempDir, arch, fmt.Sprintf("%s-%s.apk", tt.name, tt.expectedVersion))
-				apkFile, err := os.Open(apkPath)
-				if err != nil {
-					t.Fatalf("opening apk: %v", err)
-				}
-				defer apkFile.Close()
-
-				gr, err := gzip.NewReader(apkFile)
-				if err != nil {
-					t.Fatalf("creating gzip reader: %v", err)
-				}
-				defer gr.Close()
-
-				tr := tar.NewReader(gr)
-				var sbom io.Reader
-				sbomPath := fmt.Sprintf("var/lib/db/sbom/%s-%s.spdx.json", tt.name, tt.expectedVersion)
-				for {
-					hdr, err := tr.Next()
+				t.Run("sbom correctness", func(t *testing.T) {
+					apkPath := filepath.Join(tempDir, arch, fmt.Sprintf("%s-%s.apk", tt.name, tt.expectedVersion))
+					apkFile, err := os.Open(apkPath)
 					if err != nil {
-						t.Fatalf("reading tar header: %v", err)
+						t.Fatalf("opening apk: %v", err)
 					}
-					if hdr.Name == sbomPath {
-						sbom = tr
-						break
+					defer apkFile.Close()
+
+					gr, err := gzip.NewReader(apkFile)
+					if err != nil {
+						t.Fatalf("creating gzip reader: %v", err)
 					}
-				}
-				if sbom == nil {
-					t.Fatalf("SBOM not found in apk: %s", sbomPath)
-				}
+					defer gr.Close()
 
-				expectedSBOMPath := filepath.Join("testdata", "goldenfiles", "sboms", fmt.Sprintf("%s-%s.spdx.json", tt.name, tt.expectedVersion))
-				expectedSbomFile, err := os.Open(expectedSBOMPath)
-				if err != nil {
-					t.Fatalf("opening expected SBOM: %v", err)
-				}
+					tr := tar.NewReader(gr)
+					var sbom io.Reader
+					sbomPath := fmt.Sprintf("var/lib/db/sbom/%s-%s.spdx.json", tt.name, tt.expectedVersion)
+					for {
+						hdr, err := tr.Next()
+						if err != nil {
+							t.Fatalf("reading tar header: %v", err)
+						}
+						if hdr.Name == sbomPath {
+							sbom = tr
+							break
+						}
+					}
+					if sbom == nil {
+						t.Fatalf("SBOM not found in apk: %s", sbomPath)
+					}
 
-				expected, err := io.ReadAll(expectedSbomFile)
-				if err != nil {
-					t.Fatalf("reading expected SBOM: %v", err)
-				}
-				actual, err := io.ReadAll(sbom)
-				if err != nil {
-					t.Fatalf("reading actual SBOM: %v", err)
-				}
+					expectedSBOMPath := filepath.Join("testdata", "goldenfiles", "sboms", fmt.Sprintf("%s-%s.spdx.json", tt.name, tt.expectedVersion))
+					expectedSbomFile, err := os.Open(expectedSBOMPath)
+					if err != nil {
+						t.Fatalf("opening expected SBOM: %v", err)
+					}
 
-				if diff := cmp.Diff(expected, actual); diff != "" {
-					t.Fatalf("SBOMs differ: \n%s\n", diff)
-				}
+					expected, err := io.ReadAll(expectedSbomFile)
+					if err != nil {
+						t.Fatalf("reading expected SBOM: %v", err)
+					}
+					actual, err := io.ReadAll(sbom)
+					if err != nil {
+						t.Fatalf("reading actual SBOM: %v", err)
+					}
+
+					if diff := cmp.Diff(expected, actual); diff != "" {
+						t.Fatalf("SBOMs differ: \n%s\n", diff)
+					}
+				})
 			})
 		})
 	}

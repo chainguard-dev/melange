00a43a8d1dc34679be6b702deca790914c0d7ce4
Brian Murray <brian.murray@chainguard.dev>
2025-01-09 14:03:18 -0800

    Suggest adding the pkgconf test pipeline

--- pkg/sca/sca.go
+++ pkg/sca/sca.go
@@ -448,6 +448,10 @@ func generatePkgConfigDeps(ctx context.Context, hdl SCAHandle, generated *config
 		pcName := filepath.Base(path)
 		pcName, _ = strings.CutSuffix(pcName, ".pc")
 
+		if isInDir(path, []string{"usr/lib/pkgconfig/", "usr/lib64/pkgconfig/", "usr/share/pkgconfig/"}) {
+			log.Warnf("%s provides .pc files and could use a pkgconf test pipeline", hdl.PackageName())
+                }
+
 		if isInDir(path, []string{"usr/local/lib/pkgconfig/", "usr/local/share/pkgconfig/", "usr/lib/pkgconfig/", "usr/lib64/pkgconfig/", "usr/share/pkgconfig/"}) {
 			log.Infof("  found pkg-config %s for %s", pcName, path)
 			generated.Provides = append(generated.Provides, fmt.Sprintf("pc:%s=%s", pcName, hdl.Version()))

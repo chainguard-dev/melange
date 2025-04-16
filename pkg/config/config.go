31ec0faf0f55155e143428cf329442e4b92d44bc
Justin Vreeland <justin.vreeland@chainguard.com>
2025-04-15 17:36:58 -0700

    move EnablePreReleaseTags to the correct place in Update not Configuration

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -739,8 +739,6 @@ type Configuration struct {
 	VarTransforms []VarTransforms `json:"var-transforms,omitempty" yaml:"var-transforms,omitempty"`
 	// Optional: Deviations to the build
 	Options map[string]BuildOption `json:"options,omitempty" yaml:"options,omitempty"`
-	// Optional: Disables filtering of common pre-release tags
-	EnablePreReleaseTags bool `json:"enable-prerelease-tags,omitempty" yaml:"enable-prerelease-tags,omitempty"`
 
 	// Test section for the main package.
 	Test *Test `json:"test,omitempty" yaml:"test,omitempty"`
@@ -826,6 +824,8 @@ type Update struct {
 	ExcludeReason string `json:"exclude-reason,omitempty" yaml:"exclude-reason,omitempty"`
 	// Schedule defines the schedule for the update check to run
 	Schedule *Schedule `json:"schedule,omitempty" yaml:"schedule,omitempty"`
+	// Optional: Disables filtering of common pre-release tags
+	EnablePreReleaseTags bool `json:"enable-prerelease-tags,omitempty" yaml:"enable-prerelease-tags,omitempty"`
 }
 
 // ReleaseMonitor indicates using the API for https://release-monitoring.org/

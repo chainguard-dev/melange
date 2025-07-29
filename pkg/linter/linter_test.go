c5e1b77a804fc6455f812fe22a5ca695a747430a
Azimjon Ulmasov <azimjon.ulmasov@chainguard.dev>
2025-07-29 14:54:54 -0400

    linter_test.go: add packagename parameter preserving compatibility
    Signed-off-by: Azimjon Ulmasov <azimjon.ulmasov@chainguard.dev>

--- pkg/linter/linter_test.go
+++ pkg/linter/linter_test.go
@@ -19,7 +19,6 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"chainguard.dev/melange/pkg/config"
@@ -116,6 +115,7 @@ func TestLinters(t *testing.T) {
 		dirFunc func() string
 		linter  string // NB: Also the package name for this test, but that's weird!
 		pass    bool
+		pkg     string // package name (added with "maninfo" lint step)
 	}{{
 		dirFunc: t.TempDir,
 		linter:  "empty",
@@ -295,65 +295,81 @@ func TestLinters(t *testing.T) {
 		pass:    false,
 	}, {
 		dirFunc: mkfile(t, "usr/share/man/man1/foo.1"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    false,
 	}, {
 		dirFunc: mkfile(t, "usr/share/man/man1/foo.1"),
-		linter:  "maninfo-doc",
+		linter:  "maninfo",
+		pkg:     "regular-doc",
 		pass:    true,
 	}, {
 		dirFunc: mkfile(t, "usr/share/man/man8/bar.8.gz"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    false,
 	}, {
 		dirFunc: mkfile(t, "usr/local/share/man/man3/baz.3"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    false,
 	}, {
 		dirFunc: mkfile(t, "usr/man/man5/qux.5"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    false,
 	}, {
 		dirFunc: mkfile(t, "usr/share/info/test.info"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    false,
 	}, {
 		dirFunc: mkfile(t, "usr/share/info/test.info.gz"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    false,
 	}, {
 		dirFunc: mkfile(t, "usr/share/info/test.info-1"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    false,
 	}, {
 		dirFunc: mkfile(t, "usr/share/info/dir"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    false,
 	}, {
 		dirFunc: mkfile(t, "usr/share/info/test.info"),
-		linter:  "maninfo-doc",
+		linter:  "maninfo",
+		pkg:     "regular-doc",
 		pass:    true,
 	}, {
 		dirFunc: mkfile(t, "usr/bin/normal"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    true,
 	}, {
+		dirFunc: mkfile(t, "usr/local/share/info/test.info"),
+		linter:  "maninfo",
+		pkg:     "regular-package",
+		pass:    false,
+	}, {
 		dirFunc: mkfile(t, "usr/lib/libfoo.so.1"),
-		linter:  "maninfo-regular",
+		linter:  "maninfo",
+		pkg:     "regular-package",
 		pass:    true,
 	}} {
 		ctx := slogtest.Context(t)
 		t.Run(c.linter, func(t *testing.T) {
 			dir := c.dirFunc()
 
-			// Determine actual linter name
-			actualLinter := c.linter
-			if strings.HasPrefix(c.linter, "maninfo-") {
-				actualLinter = "maninfo"
+			packageName := c.pkg
+			if packageName == "" {
+				packageName = c.linter // Fallback for existing tests
 			}
 
 			// In required mode, it should raise an error.
-			err := LintBuild(ctx, c.cfg, c.linter, dir, []string{actualLinter}, nil)
+			err := LintBuild(ctx, c.cfg, packageName, dir, []string{c.linter}, nil)
 			if c.pass {
 				assert.NoError(t, err)
 			} else {
@@ -361,7 +377,7 @@ func TestLinters(t *testing.T) {
 			}
 
 			// In warn mode, it should never raise an error.
-			assert.NoError(t, LintBuild(ctx, c.cfg, c.linter, dir, nil, []string{actualLinter}))
+			assert.NoError(t, LintBuild(ctx, c.cfg, packageName, dir, nil, []string{c.linter}))
 		})
 	}
 }

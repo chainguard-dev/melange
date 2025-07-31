bc0b47827e7feea8d065045cf4f7f459d277dcf8
Azimjon Ulmasov <azimjon.ulmasov@chainguard.dev>
2025-07-31 15:51:55 -0400

    linter.go: fix regex inconsistency
    Signed-off-by: Azimjon Ulmasov <azimjon.ulmasov@chainguard.dev>

--- pkg/linter/linter.go
+++ pkg/linter/linter.go
@@ -244,7 +244,7 @@ func documentationLinter(_ context.Context, _ *config.Configuration, pkgname, pa
 
 var (
 	manRegex  = regexp.MustCompile(`(?i)^usr/(?:local/)?share/man(?:/man[0-9][^/]*)?(?:/[^/]+\.[0-9][^/]*(?:\.(?:gz|bz2|xz|lzma|Z))?)?$|^usr/man(?:/man[0-9][^/]*)?(?:/[^/]+\.[0-9][^/]*(?:\.(?:gz|bz2|xz|lzma|Z))?)?$`)
-	infoRegex = regexp.MustCompile(`(?i)^usr/(local/)?share/info/(?:dir|[^/]+\.info(?:\-[0-9]+)?(?:\.(?:gz|bz2|xz|lzma|Z))?)$`)
+	infoRegex = regexp.MustCompile(`(?i)^usr/(?:local/)?share/info/(?:dir|[^/]+\.info(?:\-[0-9]+)?(?:\.(?:gz|bz2|xz|lzma|Z))?)$`)
 )
 
 func manInfoLinter(_ context.Context, _ *config.Configuration, pkgname, path string) error {

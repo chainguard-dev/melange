chainguard-dev/melange

commit 601baee896a844d3d17b7c5611b52c28144f915b

Update pkg/container/qemu_runner.go
Co-authored-by: Evan Gibler <20933572+egibs@users.noreply.github.com>
Signed-off-by: Steve Beattie <steve+github@nxnw.org>

pkg/container/qemu_runner.go
@@ -995,7 +995,7 @@ func createMicroVM(ctx context.Context, cfg *Config) error {
 	// now that we've copied files from the host filesystem into the
 	// guest's workspace, we can umount the filesystem, as nothing in
 	// the build should reference the original location and it reduces
-	// the ability of later build steps to possibly intteract with the
+	// the ability of later build steps to possibly interact with the
 	// host.
 	err = sendSSHCommand(ctx,
 		cfg.SSHControlClient,

commit 7e104169ebc8bc825a330263c700a46472e21da7

qemu runner: make virtfs 9p bindings read-only v2 (#2155)
* qemu: virtfs 9p bindings read-only, fix copy permissions, take 2

The QEMU runner uses virtfs 9p mount bindings to inject some artifacts
into the build environment, but unlike the docker and bubblewrap
runners, does not use any bind mounts to extract out artifacts to
produce apks; instead tar over ssh is used for artifact extraction.

Thus, to reduce risk of a malicious build being able to write artifacts
outside of the QEMU guest, make the virtfs 9p bindings be read-only.
(There has also has been at least one vulnerability the virtfs 9p
implementation that allowed writes outside of the specified path.)

As part of this, start the process of moving the defaultshare 9p
mount into melange directly, rather than relying on microvm-init to do
it for us. However, because it currently does do it for us and also
makes permissions changes to the mount, we need to temporarily
compensate for this by mounting the filesystem if it's not been already
mounted or remounting it readonly if microvm-init has done it for us.

Also, because of the elimination in microvm-init of adjustment of the
permissions on the contents of the defaultshare before copying said
contents into the /home/build directory, this breaks non-root builds
where the uid of guest builder does not match the uid of the process
outside the guest invoking melange+qemu (9p's mapped permissions map
the external uid running qemu as the uid owner of the contents of the
share in the guest). Fix this by adjusting the ownership of the files
after the copy to be the user that the build will be performed under.

Finally, unmount the defaultshare 9p mount entirely after copying
the contents of it into the build directory, if the umount command is in
the guest's control environment.

Signed-off-by: Steve Beattie <steve.beattie@chainguard.dev>

* Update pkg/container/qemu_runner.go

Co-authored-by: Evan Gibler <20933572+egibs@users.noreply.github.com>
Signed-off-by: Steve Beattie <steve+github@nxnw.org>

* sbom_group_test.go: fix unrelated import linter issue

fixes: 601baee8 ("Update pkg/container/qemu_runner.go")

pkg/container/qemu_runner.go
@@ -722,11 +722,12 @@ func createMicroVM(ctx context.Context, cfg *Config) error {
 	// kill all performances (lots of small files)
 	// instead we will copy back the finished workspace artifacts when done.
 	// this dramatically improves compile time, making them comparable to bwrap or docker runners.
+	// FIXME: set readonly=on once the microvm-init script does not twiddle with /mount/mnt
 	baseargs = append(baseargs, "-fsdev", "local,security_model=mapped,id=fsdev100,path="+cfg.WorkspaceDir)
 	baseargs = append(baseargs, "-device", "virtio-9p-pci,id=fs100,fsdev=fsdev100,mount_tag=defaultshare")
 
 	if cfg.CacheDir != "" {
-		baseargs = append(baseargs, "-fsdev", "local,security_model=mapped,id=fsdev101,path="+cfg.CacheDir)
+		baseargs = append(baseargs, "-fsdev", "local,security_model=mapped,id=fsdev101,readonly=on,path="+cfg.CacheDir)
 		baseargs = append(baseargs, "-device", "virtio-9p-pci,id=fs101,fsdev=fsdev101,mount_tag=melange_cache")
 
 		// ensure the cachedir exists
@@ -913,7 +914,7 @@ func createMicroVM(ctx context.Context, cfg *Config) error {
 		setupMountCommand := fmt.Sprintf(
 			"mkdir -p %s %s /mount/upper /mount/work && "+
 				"chmod 1777 /mount/upper && "+
-				"mount -t 9p melange_cache %s && "+
+				"mount -t 9p -o ro melange_cache %s && "+
 				"mount -t overlay overlay -o lowerdir=%s,upperdir=/mount/upper,workdir=/mount/work %s",
 			DefaultCacheDir,
 			filepath.Join("/mount", DefaultCacheDir),
@@ -941,24 +942,77 @@ func createMicroVM(ctx context.Context, cfg *Config) error {
 	}
 
 	clog.FromContext(ctx).Info("qemu: setting up local workspace")
-	// This has to happen as the user we are building as, so
-	// files copied are owned by the build user, and thus cleanup
-	// step that happens at the end of the build to remove the
-	// files succeeds
+	// because microvm-init both mounts and modifies /mnt/ in the
+	// workspace, but want to get rid of that, we need to handle both
+	// remounting and first mount of the defaultshare virtfs share.
+	setupMountCommand := "if grep -q '^defaultshare ' /proc/mounts ; then " +
+		"  mount -t 9p -o remount,ro,trans=virtio defaultshare /mount/mnt ; " +
+		"else " +
+		"  mount -t 9p -o ro,trans=virtio defaultshare /mount/mnt; " +
+		"fi"
 	err = sendSSHCommand(ctx,
-		cfg.SSHBuildClient,
+		cfg.SSHControlClient,
 		cfg,
 		nil,
 		stderr,
 		stdout,
 		false,
-		[]string{"sh", "-c", "find /mnt/ -mindepth 1 -maxdepth 1 -exec cp -a {} /home/build/ \\;"},
+		[]string{"sh", "-c", setupMountCommand},
 	)
 	if err != nil {
 		err = qemuCmd.Process.Kill()
 		if err != nil {
 			return err
 		}
+	}
+
+	// This has to happen as the user we are building as, so
+	// files copied are owned by the build user, and thus cleanup
+	// step that happens at the end of the build to remove the
+	// files succeeds
+	copyFilesCommand := fmt.Sprintf(
+		"find /mnt/ -mindepth 1 -maxdepth 1 -exec cp -a {} /home/build/ \\; && "+
+			"chown -R %s /home/build",
+		cfg.SSHBuildClient.User(),
+	)
+	err = sendSSHCommand(ctx,
+		cfg.SSHControlBuildClient,
+		cfg,
+		nil,
+		stderr,
+		stdout,
+		false,
+		[]string{"sh", "-c", copyFilesCommand},
+	)
+	if err != nil {
+		err = qemuCmd.Process.Kill()
+		if err != nil {
+			return err
+		}
+	}
+
+	clog.FromContext(ctx).Info("qemu: unmounting host workspace from guest")
+	// now that we've copied files from the host filesystem into the
+	// guest's workspace, we can umount the filesystem, as nothing in
+	// the build should reference the original location and it reduces
+	// the ability of later build steps to possibly interact with the
+	// host.
+	err = sendSSHCommand(ctx,
+		cfg.SSHControlClient,
+		cfg,
+		nil,
+		stderr,
+		stdout,
+		false,
+		[]string{"sh", "-c", "if [ -x /usr/bin/umount ] ; then umount /mount/mnt ; fi"},
+	)
+	if err != nil {
+		clog.FromContext(ctx).Warnf("qemu: failed to unmount /mount/mnt in guest")
+		err = qemuCmd.Process.Kill()
+		if err != nil {
+			return err
+		}
+		// don't fail the build because of this.
 	}
 
 	cfg.QemuPID = qemuCmd.Process.Pid
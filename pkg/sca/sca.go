38ea410b16145fc57a96c7bcd1ea3981fd9d8409
Wojciech Kocjan <wojciech.kocjan@chainguard.dev>
2025-11-18 12:46:49 +0000

    fix: update after CI fail

--- pkg/sca/sca.go
+++ pkg/sca/sca.go
@@ -99,8 +99,7 @@ func isInDir(path string, dirs []string) bool {
 	mydir := filepath.Dir(path)
 	for _, d := range dirs {
 		// allow the /... suffix (i.e. "usr/...") to indicate recursive matching
-		if strings.HasSuffix(d, "/...") {
-			prefix := strings.TrimSuffix(d, "/...")
+		if prefix, ok := strings.CutSuffix(d, "/..."); ok {
 			// Ensure we match the directory exactly or as a prefix followed by /
 			// This prevents /dir matching /dirother
 			if mydir == prefix || strings.HasPrefix(mydir, prefix+"/") {

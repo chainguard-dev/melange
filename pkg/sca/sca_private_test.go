05b2b720981763b246671f329b618ac125ae3327
Dimitri John Ledkov <dimitri.ledkov@chainguard.dev>
2026-09-03 09:53:24 +0000

    sca: add support for new Chainguard Go 1.27 toolchains
    Ensure that nocgo binaries gains openssl-config-fipshardened dependency.
    Ensure that geomys capable binaries gain geomys NIST packages.
    
    Fixes OS-3002

---
+++ pkg/sca/sca_private_test.go
@@ -0,0 +1,20 @@
+// Copyright 2026 Chainguard, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build sca_private
+
+//go:generate go run ./../../ build --generate-index=false --source-dir=./testdata/go-fips-bin/ --out-dir=./testdata/generated ./testdata/go-fips-bin/go-geomys-bin.yaml --arch=x86_64 --git-commit 0000000000000000000000000000000000000000
+//go:generate go run ./../../ build --generate-index=false --source-dir=./testdata/go-fips-bin/ --out-dir=./testdata/generated ./testdata/go-fips-bin/go-msft-bin.yaml --arch=x86_64 --git-commit 0000000000000000000000000000000000000000
+
+package sca
